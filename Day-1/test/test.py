b6a16ffe82964f296030d351a4172c76280a23bf
Smittys3 <ssmith@me.com>
2022-07-06 16:42:43 -0500

    day-2-homework

---
+++ Day-1/test/test.py
@@ -0,0 +1,20 @@
+# list_num = [pow(x,3) for x in range(1, 11) if pow(x,3) % 4 == 0]
+# print(list_num)
+
+
+for num in range(1,11):
+    num += 1
+    if num == num**3:
+        print(num)
+
+    
+
+
+
+
+
+
+
+
+
+
